b25e6de1f8207323b671c1a429d3c74ef6a8cf0f
Kady <74194999+Kady2020@users.noreply.github.com>
2023-02-09 17:01:30 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Описание проекта
+[![Header](https://github.com/Kady2020/marketing-solutions/blob/main/src/img/page-link.png)](https://kady2020.github.io/marketing-solutions/dist/index.html)
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import * as flsFunctions from "./modules/webp.js";
+
+flsFunctions.isWebp();
+
+import "./libraries/jquery-3.6.0.min.js";
+import "./modules/header-burger.js";
+import "./modules/header-scroll.js";
+import "./modules/swiper.js";
